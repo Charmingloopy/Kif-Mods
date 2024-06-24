6ed9c874ac0659531f25106960074414cf0253f4
Charmingloopy <111189434+Charmingloopy@users.noreply.github.com>
2024-06-24 17:48:24 +0100

    Avaraged evo levels Update 1:
    ## Averaged Evolution Levels Update 1 Changelogs:
    Changed how much the evo levels are changed and when they are changed. Read Below for the calculations
    
    
    
    ### Calculations:
    if head evo level < body evo level - 5 or head evo level > body evo level + 4
    
    The percent to add is 5%
    
    If head evo level < body evo level - 4 or head evo level > body evo level + 3
    
    Percent to add is 10%
    
    If head evo level < body evo 
    level - 3 or head evo level > body evo level + 2
    
    Percent to add is 15% 
    
    If head evo level < body evo level - 2 or head evo level > body evo level + 1
    
    Percent to add is 20% 
    
    If head evo level < body evo level - 1 or head evo level > body evo level
    
    Percent to add is 25%
    
    if head evo level == body evo level
    
    Percent to add is 30%

--- Averaged_EvoLevels.rb
+++ Averaged_EvoLevels.rb
@@ -22,6 +22,8 @@ class Pokemon
     return nil
     end
  def round_evolevels
+
+    levelpercent = 0
     if @body_data == nil then
       @body_data = get_body_data
       end
@@ -29,12 +31,26 @@ class Pokemon
       @head_data = get_head_data
       end
     if @head_data and @body_data then
-     if @head_data[2] >= @body_data[2] - 3 and @head_data[2] <= @body_data[2] then
-       @head_data[2] += @body_data[2] * 0.3
-       @head_data[2] = @head_data[2].round()
-       end
+      if @head_data[2] < @body_data[2] - 5 or @head_data[2] > @body_data[2] + 4 then
+        levelpercent = 0.05
+      elsif @head_data[2] <  @body_data[2] - 4 or @head_data[2] >  @body_data[2] + 3 then
+          levelpercent = 0.1
+      elsif @head_data[2] <  @body_data[2] - 3 or @head_data[2] >  @body_data[2] + 2 then
+          levelpercent = 0.15
+      elsif @head_data[2] <  @body_data[2] - 2 or @head_data[2] >  @body_data[2] + 1 then
+          levelpercent = 0.2
+      elsif @head_data[2] <  @body_data[2] - 1 or @head_data[2] > @body_data[2] then
+          levelpercent = 0.25
+      elsif @head_data[2] ==  @body_data[2] then
+          levelpercent = 0.3
+      else
+        levelpercent = 0
+        end
+      @head_data[2] +=  @body_data[2] * levelpercent
+      @head_data[2] = @head_data[2].round()
+      end
   end
-end
+
 
  def check_evolution_on_level_up
     if @species_data.is_a?(GameData::FusedSpecies)
@@ -55,57 +71,59 @@ end
         success = GameData::Evolution.get(method).call_level_up(pkmn, parameter)
         next (success) ? new_species : nil
       }
-     puts head_evolution.inspect.to_s + " head evo"
      current_body = @species_data.body_pokemon
      current_head = @species_data.head_pokemon
-     if @head_data[0] != head_evolution
-        @head_data = get_head_data
+     if head_evolution then
+       if @head_data[0] != head_evolution
+         @head_data = get_head_data
+         end
         end
-     if head_evolution == nil then
-        head_evolution = @head_data[0]
-        puts head_evolution.inspect.to_s + " head evo"
-        end
-     puts @body_data.inspect
+
      if @head_data == nil then
         @head_data = get_head_data
+        round_evolevels
         end
      if @body_data == nil then
         @body_data = get_body_data
+        round_evolevels
         end
     
-     if @body_data then
+     if @body_data and body_evolution then
       if @body_data[0] != body_evolution then
          @body_data = get_body_data
+         round_evolevels
          end
       end
      round_evolevels
      if @body_data  != nil then
        if @body_data[2] < self.level then
          @body_data = get_body_data
+         round_evolevels
          end
        end
      if @head_data  != nil then
       if @head_data[2] < self.level then
         @head_data = get_head_data
+        round_evolevels
         end
       end
+      
      if @body_data != nil then
        if self.level >= @body_data[2] #EVOLVE BODY
-        if body_evolution then
+        if body_evolution != nil then
           newspecies = getFusionSpecies(body_evolution, current_head)
           end
         end
        end
      if @head_data != nil then
        if self.level >= @head_data[2] #EVOLVE HEAD
-        if head_evolution then
+        if head_evolution != nil then
          newspecies = getFusionSpecies(current_body, head_evolution)
          end
         end
        end
+     return nil if head_evolution == nil and body_evolution == nil
 
-     puts @head_data.inspect.to_s + " head data"
-     puts @body_data.inspect.to_s + " body data"
      if newspecies then
        return newspecies
       end
@@ -118,4 +136,4 @@ end
       next (success) ? new_species : nil
       }
     end
- end
+end
